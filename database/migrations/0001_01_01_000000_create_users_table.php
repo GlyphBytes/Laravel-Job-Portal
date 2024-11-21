d5e77b4baa02fcc6c9450d22182cf7fe086c936e
Abubakar <abubakarmunir712@gmail.com>
2024-11-21 23:04:20 +0500

    Added migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,13 +11,36 @@ return new class extends Migration
      */
     public function up(): void
     {
+
+        // Users table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('type',['employee','company']);
+            $table->string('contact_email')->nullable()->unique();
+            $table->string('linkedin_link')->nullable()->unique();
             $table->rememberToken();
+            $table->timestamps();
+        });
+
+        // Companies table 
+        Schema::create('companies',function(Blueprint $table){
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('company_size');
+            $table->string('address');
+            $table->longText('about_company');
+            $table->timestamps();
+        });
+
+        // Employees table 
+        Schema::create('employees',function(Blueprint $table){
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('cv')->nullable();
+            $table->enum('gender',['male','female','other']);
             $table->timestamps();
         });
 
